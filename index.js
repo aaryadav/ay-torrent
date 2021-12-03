2c2d80e59613656598c879b1c5ab00f30841697d
aaryaman <aaryaxb1@gmail.com>
2021-12-03 16:36:39 +0530

    add torrent parser

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const bencode = require('bencode');
 const tracker = require('./tracker');
+const torrent = require('./torrent-parser');
 
 const torrent = bencode.decode(fs.readFileSync('ship.torrent'));
 
